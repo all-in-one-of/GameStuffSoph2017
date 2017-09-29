99cbf9931b71ee891927ee3e523e065cbc66e340
Unknown <sam.corbridge@gmail.com>
2017-09-28 18:36:04 -0600

    lightpuzzles working

--- Game Project/Assets/Scripts/LightScript.cs
+++ Game Project/Assets/Scripts/LightScript.cs
@@ -6,20 +6,32 @@ using System;
 public class LightScript : MonoBehaviour {
 
 
-	public LayerMask myLayerMask;
+
 	public float theDistance;
 	RaycastHit Hit;
+	public Transform target;
+	public Transform MoveMe;
+	public float speed;
 	void Start () {
 
 	}
 	
 	void Update () {
 		Vector3 pointMe = transform.TransformDirection(Vector3.forward) * 10;
-		Debug.DrawRay(transform.position, pointMe, Color.green);
-		if(Physics.Raycast(transform.position,(pointMe), out Hit)){
+			Debug.DrawRay(transform.position, pointMe, Color.green);
+
+		if(Physics.Raycast(transform.position,(pointMe), out Hit, 10)){
 			theDistance = Hit.distance;
-			print(theDistance + " " + Hit.collider.gameObject.name);
+			if(Hit.transform.tag == "score"){
+				float step = speed * Time.deltaTime;
+        		MoveMe.transform.position = Vector3.MoveTowards(MoveMe.transform.position, target.position, step);
+			}
+
+
 		}
 
+
 	}
+
 }
+
---
+++ Game Project/Assets/Scripts/PatrollingEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PatrollingEnemy : MonoBehaviour {
+
+
+public int direction = 1;
+public float speed;
+
+
+
+	void Update(){	
+		// transform.position = Vector3.MoveTowards(meme.transform.position, point2.position, movespeed);
+		transform.Translate(Vector3.forward*speed*direction*Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "berry"){
+		if(direction == 1){
+			direction = -1;
+		}
+		else{
+			direction = 1;
+		}
+		}
+	}
+}
